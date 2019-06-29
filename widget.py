99271b1fb3ea3420838e70f93b62807de968db30
Eric Romrell <eric_romrell@byu.edu>
2019-06-29 14:29:40 -0600

    Added all functions to widget

--- widget.py
+++ widget.py
@@ -5,11 +5,27 @@ class Widget:
         self.name = name
         self.server = server
 
+    ### DISPLAY WIDGET
+
     def update_text(self, text):
         self.raise_event("UPDATE_TEXT", text)
 
+    ### BUTTON WIDGET
+
+    def update_button_text(self, text):
+        self.raise_event("UPDATE_BUTTON_TEXT", text)
+
+    ### AUDIO WIDGET
+
     def speak(self, text):
         self.raise_event("SPEAK", text)
+
+    ### KNOB WIDGET
+
+    def update_knob(self, progress):
+        self.raise_event("UPDATE_KNOB_PROGRESS", progress)
+
+    ### PRIVATE FUNCTIONS
 
     def raise_event(self, event_type, message):
         event = {
