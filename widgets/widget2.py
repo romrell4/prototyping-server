9ee2a02bf1eb174a62f36467a85383c521534979
Eric Romrell <eric_romrell@byu.edu>
2019-07-04 13:50:27 -0600

    Fixed knob handler

--- widgets/widget2.py
+++ widgets/widget2.py
@@ -1,2 +1,2 @@
-def knob_progress_updated(widgets, new_progress):
+def knob_progress_updated(widgets, state, new_progress):
     widgets["widget3"].speak("Widget 2 is now at {} percent!".format(new_progress))
